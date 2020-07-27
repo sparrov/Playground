ebd3987653343c01b70668ad9a3f76a9e71b9c64
Szymon <sparrov@gmail.com>
2020-07-27 20:46:38 +0200

    Initial commit

---
+++ src/main/java/pl/sw/Listing_6_21_NestedClass/Outer.java
@@ -0,0 +1,49 @@
+package pl.sw.Listing_6_21_NestedClass;
+
+public class Outer {
+    private int[] numbers;
+
+    public Outer(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    public void calculate() {
+        Inner inner = new Inner();
+        System.out.println("Wartość min: " + inner.min());
+        System.out.println("Wartość max: " + inner.max());
+        System.out.println("Srednia: " + inner.avg());
+    }
+
+    class Inner {
+
+        private int min() {
+            int min = numbers[0];
+            for (int i = 0; i < numbers.length; i++) {
+                if (numbers[i] < min) {
+                    min = numbers[i];
+                }
+            }
+            return min;
+        }
+
+        private int max() {
+            int max = numbers[0];
+            for (int i = 0; i < numbers.length; i++) {
+                if (numbers[i] > max) {
+                    max = numbers[i];
+                }
+            }
+            return max;
+        }
+
+        private double avg() {
+            int sum = 0;
+            double avg;
+            for (int i = 0; i < numbers.length; i++) {
+                sum += numbers[i];
+            }
+            avg = sum / numbers.length;
+            return avg;
+        }
+    }
+}
---
+++ src/main/java/pl/sw/Listing_6_23_6_24_6_25_VarArgs/VarArgs3.java
@@ -0,0 +1,33 @@
+package pl.sw.Listing_6_23_6_24_6_25_VarArgs;
+
+public class VarArgs3 {
+    public static void main(String[] args) {
+        varArgsTest("Liczba argumentów: ", 1, 2, 3);
+        varArgsTest("Liczba argumentów: ", true, true, false);
+        varArgsTest("Liczba argumentów: ", 'a', 'b', 'c');
+    }
+
+    public static void varArgsTest(String message, int... args) {
+        System.out.println(message + args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argument " + i + ": " + args[i]);
+        }
+        System.out.println();
+    }
+
+    public static void varArgsTest(String message, boolean... args) {
+        System.out.println(message + args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argument " + i + ": " + args[i]);
+        }
+        System.out.println();
+    }
+
+    public static void varArgsTest(String message, char... args) {
+        System.out.println(message + args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argument " + i + ": " + args[i]);
+        }
+        System.out.println();
+    }
+}
